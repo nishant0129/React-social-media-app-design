d46adde66e1b7e8bef7633413587c11ff77e751c
Nishant Kumar <nishantkumar222555@gmail.com>
2023-01-13 23:34:40 +0530

    First commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,44 @@
+import "./topbar.css"
+import Notifications from '@mui/icons-material/NotificationsActive';
+import Chat from '@mui/icons-material/Chat';
+import Person from '@mui/icons-material/Person';
+import Search from '@mui/icons-material/Search';
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">social</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/7.jpg" alt="" className="topbarImg"/>
+      </div>
+    </div>
+  );
+}
